29646ee41478b23bbda941d857c16a242bc037b6
vrma8 <24bcs096@nith.ac.in>
2026-09-04 18:25:00 +0530

    pacaging issue

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const rootDir = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
     unoptimized: true,
   },
